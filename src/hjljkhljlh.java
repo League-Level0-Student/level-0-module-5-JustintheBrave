e87377675469a8ae0fb98484b60b78ae90cfc884
JustintheBrave <55635392+JustintheBrave@users.noreply.github.com>
2020-04-10 17:03:15 -0700

    ff
    ff

---
+++ src/hjljkhljlh.java
@@ -0,0 +1,12 @@
+
+public class hjljkhljlh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 6; i >=-1; i--) {
+			System.out.println(i);
+		}
+		}
+	}
+
+
