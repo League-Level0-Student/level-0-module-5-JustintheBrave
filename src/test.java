7aadcc3171a0edaa14c1afab2386988ad6b83803
JustintheBrave <55635392+JustintheBrave@users.noreply.github.com>
2020-04-10 17:03:01 -0700

    hi
    afada

---
+++ src/test.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+import org.jointheleague.graphical.robot.RobotImage;
+
+public class test {
+
+	public static void main(String[] args) {
+String test = JOptionPane.showInputDialog("what did you get on the test?");
+JOptionPane.showMessageDialog(null, "noob you only got " + test + "" + "?");
+	}
+}
